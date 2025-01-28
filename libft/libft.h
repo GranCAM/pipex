0ad0018fccb2fd5cbabc4e22a756c6d19f005ccb
Carlos Arbon Miguel <yo.nadie.relevante@gmail.com>
2025-01-28 16:11:41 +0100

    new libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: carbon-m <carbon-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 12:48:04 by carbon-m          #+#    #+#             */
-/*   Updated: 2024/10/07 13:09:30 by carbon-m         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:11:32 by carbon-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void	ft_frematrix(char **all);
 
 #endif
